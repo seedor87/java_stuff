9bfda2b7755cd8085b01f61e28424d701d2683b4
rjseedorf <seedor87@students.rowan.edu>
2017-09-01 20:31:32 -0400

    fixed state transitions in the AbstractTimer, TODO test against existing uses throughout code

--- src/myUtils/Measurement/AbstractTimer.java
+++ src/myUtils/Measurement/AbstractTimer.java
@@ -2,18 +2,30 @@ package myUtils.Measurement;
 
 import myUtils.ConsolePrinting;
 
-import java.awt.*;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public abstract class AbstractTimer {
 
+    class IllegalStateTransitionExcpetion extends RuntimeException {
+
+        IllegalStateTransitionExcpetion(String msg) {
+            super(msg);
+        }
+    }
+
     public enum TimeUnit {
         NANO, MICRO, MILLI, SECONDS, MINUTES, HOURS
     }
+
+    public enum State {
+        STOPPED, STARTED, SUSPENDED, RESUMMED
+    }
+
     private TimeUnit unit = TimeUnit.MILLI;
     double startTime = 01, endTime = 01, elapsedTime = 0;
     DecimalFormat formatter = new DecimalFormat("#,###,###.###");
+    private State current = State.STOPPED;
 
     public abstract double getTime();
     public abstract double getElapsedTime(TimeUnit u);
@@ -51,35 +63,45 @@ public abstract class AbstractTimer {
     public double start () {
         startTime = getTime();
         elapsedTime = 0;
+        current = State.STARTED;
         return startTime;
     }
 
     public double suspend() {
-        endTime = getTime();
-        if (!isRunning() ) {
-            endTime = 01;
-        } else {
+        double hold = getTime();
+        if(current == State.STARTED || current == State.RESUMMED) {
+            endTime = hold;
             elapsedTime += (endTime - startTime);
             startTime = 0l;
+            current = State.SUSPENDED;
+            return endTime;
         }
-        return endTime;
+        endTime = 01;
+        throw new IllegalStateTransitionExcpetion(current + " -> " + State.SUSPENDED);
     }
 
     public double resume() {
-        double temp = getTime();
-        if(!isRunning() && endTime != 01) {
-            startTime = temp;
+        double hold = getTime();
+        if(current == State.SUSPENDED) {
+            startTime = hold;
+            endTime = 01;
+            current = State.RESUMMED;
+            return startTime;
         }
-        return temp;
+        throw new IllegalStateTransitionExcpetion(current + " -> " + State.RESUMMED);
     }
 
     public double stop () {
-        endTime = getTime();
-        if(!isRunning()) {
-            elapsedTime = 0;
-        } else {
+        double hold = getTime();
+        if(current == State.RESUMMED || current == State.STARTED) {
+            endTime = hold;
             elapsedTime += (endTime - startTime);
             startTime = 0l;
+            current = State.STOPPED;
+        } else {
+            elapsedTime = 0;
+            startTime = 0;
+            endTime = 0;
         }
         return endTime;
     }
@@ -120,39 +142,43 @@ public abstract class AbstractTimer {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String key = "start[1] suspend[2] resume[3] stop[4]";
-        ConsolePrinting.COLOR printCol = ConsolePrinting.COLOR.RED;
-        int input;
+        int input = 4;
         AbstractTimer timer = new SYSTimer(TimeUnit.SECONDS);
         String lastAction = "timer application";
+        ConsolePrinting.println(ConsolePrinting.COLOR.RED, timer + " : " + lastAction);
         do {
-            ConsolePrinting.println(printCol, timer + " : " + lastAction);
             ConsolePrinting.println(key);
             ConsolePrinting.print(">> ");
             String s = reader.next();
             input = Integer.parseInt(s);
-            switch (input) {
-                case 1:
-                    timer.start();
-                    lastAction = "started";
-                    printCol = ConsolePrinting.COLOR.GREEN;
-                    break;
-                case 2:
-                    timer.suspend();
-                    lastAction = "suspended";
-                    printCol = ConsolePrinting.COLOR.YELLOW;
-                    break;
-                case 3:
-                    timer.resume();
-                    lastAction = "resumed";
-                    printCol = ConsolePrinting.COLOR.CYAN;
-                    break;
-                case 4:
-                    timer.stop();
-                    lastAction = "stopped";
-                    printCol = ConsolePrinting.COLOR.RED;
-                    break;
-                default:
-                    break;
+            try {
+                switch (input) {
+                    case 1:
+                        timer.start();
+                        lastAction = "started";
+                        ConsolePrinting.println(ConsolePrinting.COLOR.GREEN, timer + " : " + lastAction);
+                        break;
+                    case 2:
+                        timer.suspend();
+                        lastAction = "suspended";
+                        ConsolePrinting.println(ConsolePrinting.COLOR.YELLOW, timer + " : " + lastAction);
+                        break;
+                    case 3:
+                        timer.resume();
+                        lastAction = "resumed";
+                        ConsolePrinting.println(ConsolePrinting.COLOR.CYAN, timer + " : " + lastAction);
+
+                        break;
+                    case 4:
+                        timer.stop();
+                        lastAction = "stopped";
+                        ConsolePrinting.println(ConsolePrinting.COLOR.RED, timer + " : " + lastAction);
+                        break;
+                    default:
+                        break;
+                }
+            } catch (IllegalStateTransitionExcpetion ex) {
+                ConsolePrinting.println(ConsolePrinting.COLOR.PURPLE, ex.toString());
             }
         } while(0 < input && input < 5);
         ConsolePrinting.print("quiting...");
